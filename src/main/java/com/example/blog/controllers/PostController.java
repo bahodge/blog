bc08b95c4b2c71350c815fa2af1c5b9a5dd0e125
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-04-18 15:05:46 -0500

    added basic create controller

--- src/main/java/com/example/blog/controllers/PostController.java
+++ src/main/java/com/example/blog/controllers/PostController.java
@@ -1,43 +1,42 @@
 package com.example.blog.controllers;
 
 import com.example.blog.models.Post;
+import com.example.blog.services.PostService;
+import javafx.geometry.Pos;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class PostController {
+    PostService postSvc;
+
+    public PostController(PostService postSvc) {
+        this.postSvc = postSvc;
+    }
+
     @GetMapping (path = "/posts")
     public String posts(Model model){
-        List<Post> posts = new ArrayList<>();
-        Post p1 = new Post("Post 1", "Post 1 body is so cool!!!");
-        Post p2 = new Post("Post 2", "Post 2 Body is super neat!");
-        posts.add(p1);
-        posts.add(p2);
-        model.addAttribute("posts", posts);
+        model.addAttribute("posts", postSvc.getAllPosts());
         return "posts/index";
     }
 
     @GetMapping (path = "/posts/{id}")
-    public String onePost(Model model, @PathVariable int id){
-        Post post = new Post("My First Post", "This post is so awesome!!!");
-        model.addAttribute("post", post);
+    public String show(Model model, @PathVariable long id){
+        model.addAttribute("post", postSvc.getPost(id));
         return "posts/show";
     }
 
-    @RequestMapping (path = "/posts/create", method = RequestMethod.GET)
-    @ResponseBody
-    public String getCreate(){
-        return "'Create' Get Request";
+    @GetMapping (path = "/posts/create")
+    public String getCreate(Model model){
+        model.addAttribute("post", new Post());
+        return "posts/create";
     }
 
-    @RequestMapping (path = "/posts/create", method = RequestMethod.POST)
-    @ResponseBody
-    public String createPost(){
-        return "Create a new post";
+    @PostMapping(path = "/posts/create")
+    public String createPost(@ModelAttribute Post post){
+        postSvc.save(post);
+        return "redirect:/posts";
     }
 
 
--- src/main/java/com/example/blog/models/Post.java
+++ src/main/java/com/example/blog/models/Post.java
@@ -2,8 +2,10 @@ package com.example.blog.models;
 
 public class Post {
 
+    private long id;
     private String title;
     private String body;
+
 
     public Post() {
     }
@@ -13,6 +15,12 @@ public class Post {
     }
 
     public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public Post(long id, String title, String body) {
+        this.id = id;
         this.title = title;
         this.body = body;
     }
@@ -29,12 +37,11 @@ public class Post {
         this.title = title;
     }
 
-
-
     public void setBody(String body) {
         this.body = body;
     }
 
+    public long getId() { return id; }
 
-
+    public void setId(long id) { this.id = id; }
 }
