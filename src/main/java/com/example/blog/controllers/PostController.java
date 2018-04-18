034a9568de0fe1680364bad42d94ecb772c5cd91
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-04-18 16:06:54 -0500

    finished edit function without database

--- src/main/java/com/example/blog/controllers/PostController.java
+++ src/main/java/com/example/blog/controllers/PostController.java
@@ -15,18 +15,21 @@ public class PostController {
         this.postSvc = postSvc;
     }
 
+//    All Posts
     @GetMapping (path = "/posts")
     public String posts(Model model){
         model.addAttribute("posts", postSvc.getAllPosts());
         return "posts/index";
     }
 
+//  Individual Post
     @GetMapping (path = "/posts/{id}")
     public String show(Model model, @PathVariable long id){
         model.addAttribute("post", postSvc.getPost(id));
         return "posts/show";
     }
 
+//    Create a post
     @GetMapping (path = "/posts/create")
     public String getCreate(Model model){
         model.addAttribute("post", new Post());
@@ -36,6 +39,21 @@ public class PostController {
     @PostMapping(path = "/posts/create")
     public String createPost(@ModelAttribute Post post){
         postSvc.save(post);
+        return "redirect:/posts";
+    }
+
+//    Editing a post
+    @GetMapping(path = "/posts/{id}/edit")
+    public String getEdit(Model model, @PathVariable long id){
+        model.addAttribute("post", postSvc.getPost(id));
+        return "/posts/edit";
+    }
+
+    @PostMapping(path = "/posts/edit")
+    public String handleEdit(@ModelAttribute Post post){
+        Post e = postSvc.getPost(post.getId());
+        e.setTitle(post.getTitle());
+        e.setBody(post.getBody());
         return "redirect:/posts";
     }
 
